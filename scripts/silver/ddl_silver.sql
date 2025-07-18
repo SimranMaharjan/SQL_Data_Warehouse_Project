ebacd613416942bf9b9122c4db32b3937196be30
SimranMaharjan <97386018+SimranMaharjan@users.noreply.github.com>
2025-07-18 15:15:29 +0545

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -54,7 +54,7 @@ CREATE TABLE silver.crm_prd_info(
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
-	cid	NVARCHAR(50),
+	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
